7f1340c8224a80595a0eb388f3c35d9610f6f4d8
winfredo <winfrednukpe2002@gmail.com>
2025-11-06 18:14:29 +0000

    fixes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import Image from "next/image";
 
-const page = () => {
+export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="bg-linear-to-br from-[#f0f4ff] via-[#ffffff] to-[#e6f0ff]  flex flex-col items-center justify-center min-h-screen text-white">
+
+      <div className="text-center px-6">
+        <h1 className="text-2xl md:text-5xl font-bold mb-4 text-black">
+        <Image
+            src="/images/earth.png"
+            alt="GPS Tracker Logo"
+            width={60}
+            height={60}
+            className="inline-block mr-4"
+          />
+          Real-Time GPS Tracker
+        </h1>
+        <p className="text-sm md:text-lg text-gray-400 mb-8">
+          Track your movement live and view it anywhere in real time.
+        </p>
+
+        <div className="flex flex-wrap gap-4 justify-center">
+          <Link
+            href="/tracker"
+            className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-xl shadow-lg transition"
+          >
+            🚗 Go to Tracker
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -2,59 +2,72 @@
 import React, { useState, useEffect } from "react";
 import { db } from "@/firebase";
 import { doc, setDoc, getDoc } from "firebase/firestore";
+import { FaLocationDot } from "react-icons/fa6";
+import Image from 'next/image'
 
-type Coords = {
+
+type Coords ={
   lat: number | null;
   lon: number | null;
 };
 
 const page = () => {
-    const [coords, setCoords] = useState<Coords>({ lat: null, lon: null });
+  const [coords, setCoords] = useState<Coords>({ lat: null, lon: null });
 
-useEffect(() => {
-  if (!navigator.geolocation) {
-    alert("Geolocation is not supported by this device");
-    return;
-  }
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      alert("Geolocation is not supported by this device");
+      return;
+    }
 
-  const updateLoc = () => {
-    navigator.geolocation.getCurrentPosition(
-      async (position) => {
-        const { latitude, longitude } = position.coords;
-        setCoords({ lat: latitude, lon: longitude });
+    const updateLoc = () => {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          setCoords({ lat: latitude, lon: longitude });
 
-        await setDoc(doc(db, "locations", "device"), {
-          lat: latitude,
-          lon: longitude,
-          timestamp: new Date(),
-        });
-      },
-      (error) => {
-        console.error("Error obtaining location", error);
-      }
-    );
-  };
-  updateLoc();
-
-  const interval = setInterval(updateLoc, 5000);
-  return () => clearInterval(interval);
-},[]);
-    
-  return (
-    <div>
-        <h2>GPS Tracker</h2>
-        {
-            coords.lat && coords.lon ? (
-                <>
-                <p>Latitude: {coords.lat.toFixed(4)} </p>
-                <p> Longitude: {coords.lon.toFixed(4)}</p>
-                </>
-            ) : (
-                <p>Obtaining location...</p>
-            )
+          await setDoc(doc(db, "locations", "device"), {
+            lat: latitude,
+            lon: longitude,
+            timestamp: new Date(),
+          });
+        },
+        (error) => {
+          console.error("Error obtaining location", error);
         }
+      );
+    };
+    updateLoc();
+
+    const interval = setInterval(updateLoc, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="w-full h-screen flex justify-center gap-7 items-center">
+     <div className="w-[35%] h-[50%] rounded-lg overflow-hidden bg-white relative">
+  <Image
+    src="/images/map.jpeg"
+    alt="Map"
+    fill
+    className="object-cover"
+  />
+</div>
+      <div className="w-[17%] h-[40%]  flex flex-col items-left bg-gray-200 rounded-lg shadow-md p-6">
+        <h2 className="text-2xl font-bold mb-16"><FaLocationDot className="inline-block" />GPS Tracker</h2>
+        <p className="text-[20px]">Status</p>
+        <p className="font-bold mb-5">{coords.lat && coords.lon ? "Online" : "Offline"}</p>
+        {coords.lat && coords.lon ? (
+          <div className="text-[20px]">
+            <p className="font-bold">Latitude</p> <p>{coords.lat.toFixed(4)} </p>
+            <p className="font-bold mt-5">Longitude</p> <p>{coords.lon.toFixed(4)}</p>
+          </div>
+        ) : (
+          <p>Obtaining location...</p>
+        )}
+      </div>
     </div>
-  )
+  );
 };
 
 export default page;
