2ca2cdeb3720f153b6fdf53b93c1e9e765d2dcbe
winfredo <winfrednukpe2002@gmail.com>
2025-11-06 18:28:18 +0000

    fixes

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -3,10 +3,9 @@ import React, { useState, useEffect } from "react";
 import { db } from "@/firebase";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { FaLocationDot } from "react-icons/fa6";
-import Image from 'next/image'
+import Image from "next/image";
 
-
-type Coords ={
+type Coords = {
   lat: number | null;
   lon: number | null;
 };
@@ -45,22 +44,24 @@ const page = () => {
 
   return (
     <div className="w-full h-screen flex justify-center gap-7 items-center">
-     <div className="w-[35%] h-[50%] rounded-lg overflow-hidden bg-white relative">
-  <Image
-    src="/images/map.jpeg"
-    alt="Map"
-    fill
-    className="object-cover"
-  />
-</div>
+      <div className="w-[35%] h-[50%] rounded-lg overflow-hidden bg-white relative">
+        <Image src="/images/map.jpeg" alt="Map" fill className="object-cover" />
+      </div>
       <div className="w-[17%] h-[40%]  flex flex-col items-left bg-gray-200 rounded-lg shadow-md p-6">
-        <h2 className="text-2xl font-bold mb-16"><FaLocationDot className="inline-block" />GPS Tracker</h2>
+        <h2 className="text-2xl font-bold mb-16">
+          <FaLocationDot className="inline-block" />
+          GPS Tracker
+        </h2>
         <p className="text-[20px]">Status</p>
-        <p className="font-bold mb-5">{coords.lat && coords.lon ? "Online" : "Offline"}</p>
+        <p className="font-bold mb-5">
+          {coords.lat && coords.lon ? "Online" : "Offline"}
+        </p>
         {coords.lat && coords.lon ? (
           <div className="text-[20px]">
-            <p className="font-bold">Latitude</p> <p>{coords.lat.toFixed(4)} </p>
-            <p className="font-bold mt-5">Longitude</p> <p>{coords.lon.toFixed(4)}</p>
+            <p className="font-bold">Latitude</p>{" "}
+            <p>{coords.lat.toFixed(4)} </p>
+            <p className="font-bold mt-5">Longitude</p>{" "}
+            <p>{coords.lon.toFixed(4)}</p>
           </div>
         ) : (
           <p>Obtaining location...</p>
